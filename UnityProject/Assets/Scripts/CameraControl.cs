bd108bd97352bd226090104d5b915c560271a0e2
clair5610 <58032072+clair5610@users.noreply.github.com>
2019-12-26 21:37:42 +0800

    角色轉向及攝影機跟隨

---
+++ UnityProject/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+namespace MyNamespace
+{
+    public class CameraControl : MonoBehaviour
+    {
+        [Header("速度"), Range(0, 100)]
+        public float speed = 1.5f;
+        [Header("上方限制")]
+        public float top;
+        [Header("下方限制")]
+        public float bottom;
+
+        private Transform player;
+
+        private void Start()
+        {
+            player = GameObject.Find("女孩").transform;
+        }
+
+        //在 Update 之後才會執行，可用在 = 攝影機追蹤、物件追蹤
+        private void LateUpdate()
+        {
+            Track();
+        }
+
+        /// <summary>
+        /// 攝影機追蹤效果
+        /// </summary>
+        private void Track()
+        {
+            Vector3 posP = player.position; // 玩家
+            Vector3 posC = transform.position; // 攝影機
+
+            posP.x = 0;    // 固定x軸
+            posP.y = 25;   // 固定y軸
+            posP.z += 29;  //放在玩家後方 (+或-)= 29
+
+            posP.z = Mathf.Clamp(posP.z, bottom, top);  // 玩家.Z軸 夾住在 上方限制~下方限制
+
+            // 攝影機.座標 = 三維插值(A座標，B座標，百分比(攝影機移動速度)
+            transform.position = Vector3.Lerp(posC, posP, 0.3f * Time.deltaTime * speed);
+        }
+    }
+}
+
--- UnityProject/Assets/Scripts/player.cs
+++ UnityProject/Assets/Scripts/player.cs
@@ -7,15 +7,20 @@ public class player : MonoBehaviour
     [Header("速度"), Range(0, 1500)]
     public float speed = 1.5f;
 
+    
+
     private Rigidbody rig;
     private FixedJoystick Joystick;
     private Animator ani; // 動畫控制器元件
+    private Transform target; //目標物件
 
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
         ani = GetComponent<Animator>(); // 動畫控制器 = 取得元件<動畫控制器>()
         Joystick = GameObject.Find("虛擬搖桿").GetComponent<FixedJoystick>();
+        //target = GameObject.Find("虛擬搖桿").GetComponent<Transform>();
+        target = GameObject.Find("目標").transform;
     }
 
     // 固定更新 : 一秒執行 50 次 - 處理物理行為
@@ -37,6 +42,28 @@ public class player : MonoBehaviour
         rig.AddForce(-h * speed, 0, -v * speed);
 
         ani.SetBool("跑步開關", v != 0 || h != 0); // 動畫控制器.設定布林值("參數名稱"，布林值)
+
+        Vector3 pos = transform.position; // 玩家目標 = 變形.座標
+        target.position = new Vector3(pos.x - h, 1.09F, pos.z - v); // 目標.座標 = 新 三維向量(玩家.x + 水平,0.3f, 玩家.z + 垂直)
+
+        //transform.LookAt(target); ///直接這樣寫會吃土(角色靜止時無法站立，但行走正常)
+
+        Vector3 posTarget = new Vector3(target.position.x, transform.position.y, target.position.z);
+        transform.LookAt(posTarget);
     }
 
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
