dfeda5cbc78b9cfa8fe303d98e81eed4e3294fd5
clair5610 <58032072+clair5610@users.noreply.github.com>
2020-02-25 20:50:59 +0800

    結束啦啦啦

---
+++ UnityProject/Assets/Scripts/MuneManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MuneManager : MonoBehaviour
+{
+    [Header("玩家資料")]
+    public playerData dataPlayer;
+
+    public void BuyHp_500()
+    {
+        dataPlayer.hpMax += 500;           // 最大值遞增
+        dataPlayer.hp = dataPlayer.hpMax;
+    }
+
+    public void BuyAtk_50()
+    {
+        dataPlayer.attack += 50;           // 攻擊力遞增
+    }
+}
